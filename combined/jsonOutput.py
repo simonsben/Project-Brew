a1e4bd2f6bc10a7d8a82251868e65ed7ecfeefb7
Ben <simons.ben0@gmail.com>
2017-03-14 21:30:03 -0400

    added JSON output

---
+++ combined/jsonOutput.py
@@ -0,0 +1,10 @@
+import json
+import beerClass
+
+
+class dataToFile:
+        def toJSON(self, listOfBeer):
+            beerObject = beerClass.Beer
+            jsonString = json.dumps([beerObject.__dict__ for beerObject in listOfBeer.list])
+            print(jsonString)
+            return jsonString
--- combined/main.py
+++ combined/main.py
@@ -1,5 +1,6 @@
 import beerClass
 import functionClass
+import jsonOutput
 
 #Class Instantiation
 typeClass = beerClass.Beer
@@ -20,9 +21,15 @@ listOfBeer = funcClass.ripList(funcClass, link)
 #Sort list of beers by mL / $
 listOfBeer.sort('valueAlcohol')
 
+#Output list of beers to text summary
 with open('dataTaken.txt', 'w+') as f:
     for i in range(0, listOfBeer.length):
         print(listOfBeer.prntSng(i), file=f)
 
+#Output list of beers to JSON
+with open('jsonOut.txt', 'w+') as f:
+    print(jsonOutput.dataToFile.toJSON(jsonOutput.dataToFile, listOfBeer), file=f)
+
 #Print list of beers
 listOfBeer.prnt()
+

afe966d54158e1ba54ffa4c19dffff769902604c
simonsben <simons.ben0@gmail.com>
2019-10-07 17:12:05 -0400

    Removed old files

--- old_files/jsonOutput.py
+++
@@ -1,8 +0,0 @@
-import json
-from old_files import beerClass
-
-
-class dataToFile:
-        def toJSON(self, listOfBeer):
-            jsonString = json.dumps([beerClass.Beer.__dict__ for beerClass.Beer in listOfBeer.list])
-            return jsonString
